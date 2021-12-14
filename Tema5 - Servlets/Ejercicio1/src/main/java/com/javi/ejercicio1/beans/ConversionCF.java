be614650ba7f6ad775682d446e3afbd780067813
javi-de <javier_de@hotmail.com>
2021-12-14 11:57:46 +0100

    ACTUALZIACION EJERCICIO1

---
+++ Tema5 - Servlets/Ejercicio1/src/main/java/com/javi/ejercicio1/beans/ConversionCF.java
@@ -0,0 +1,37 @@
+package com.javi.ejercicio1.beans;
+
+public class ConversionCF {
+        private double celsius;
+        private double fahrenheit;
+        
+        
+        public ConversionCF(double temp, char tipoTemp){
+        //tipoTemp: 'c' para recoger celsius, 'f' para recoger fahrenheit
+            if(tipoTemp== 'c'){
+                celsius= temp;
+                fahrenheit= Cel_to_Fah(temp);
+            }
+            
+            if(tipoTemp== 'f'){
+                fahrenheit= temp;
+                celsius= Fah_to_Cel(temp);            }
+        }
+        
+        //conversores de temperaturas
+        private double Cel_to_Fah(double temp){
+            return ( (temp*9/5) + 32); //devuelve en Fahrenheit
+        }
+        
+        private double Fah_to_Cel(double temp){
+            return ( (temp - 32)*5/9  ); //devuelve en Celsius
+        }
+        
+        //getters
+        public double getCelsius(){
+            return celsius;
+        }
+        
+        public double getFahrenheit(){
+            return fahrenheit;
+        }
+}
--- Tema5 - Servlets/Ejercicio1/src/main/java/com/javi/ejercicio1/servlets/ServletConversor.java
+++ Tema5 - Servlets/Ejercicio1/src/main/java/com/javi/ejercicio1/servlets/ServletConversor.java
@@ -22,11 +22,38 @@ public class ServletConversor extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        
+        if(request.getParameter("butCelFah")!= null){
+            String strCelsius = request.getParameter("celsius");
+            
+            System.out.println("Procesando petición.");
+            
+            if(strCelsius == null || strCelsius.equals("")){
+                System.out.println("Generando página error");
+                crearPáginaError(request, response, "No ha introducido ningún valor");
+                return;
+            }
+            
+            try{
+                 double celsius= 
+                
+                
+            }catch(NumberFormatException e){
+                
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        /*
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -37,8 +64,39 @@ public class ServletConversor extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
         }
+        */
+    }
+    
+    
+    
+    private void crearPáginaError(HttpServletRequest request, HttpServletResponse response, String mensajeError) throws ServletException, IOException{
+    response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletConversor</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h2>" + mensajeError + "</h2>");
+            out.println("<a href='http://localhost:8080/Ejercicio1/'>Volver al formulario</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    
     }
 
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
